e01da197811d5054f357833d8f1760a88be4409c
Vasilii <Landing90@mail.ru>
2022-10-02 20:28:31 +0400

    oon

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Praktikum {
+    public static void main(String[] args) {
+        Hamster homka = new Hamster();
+        // Присвойте полям новые значения
+        
+
+        System.out.println("Имя: " + homka.name);
+        System.out.println("Возраст: " + homka.age);
+        System.out.println("Вес: " +  homka.weight);
+        System.out.println("Цвет: " +  homka.color);
+    }
+}
